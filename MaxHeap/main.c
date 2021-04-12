0e06f90ac69265fe840ffa4349c64c414c319814
chen-qingyu <236521827@qq.com>
2021-04-13 04:36:51 +0800

    update

--- MaxHeap/main.c
+++ MaxHeap/main.c
@@ -7,16 +7,18 @@ int main(void)
     printf("Is the heap empty? %s\n", IsEmpty(heap) ? "Yes" : "No");
     printf("\n");
 
-    printf("Insert data: 1, 2, 3.\n");
+    printf("Insert data: 1, 2, 4, 3.\n");
     Insert(heap, 1);
     Insert(heap, 2);
+    Insert(heap, 4);
     Insert(heap, 3);
     printf("\n");
 
     printf("Is the heap empty? %s\n", IsEmpty(heap) ? "Yes" : "No");
     printf("\n");
 
-    printf("Delete three items.\n");
+    printf("Delete 4 items.\n");
+    printf("The data is %d.\n", Delete(heap));
     printf("The data is %d.\n", Delete(heap));
     printf("The data is %d.\n", Delete(heap));
     printf("The data is %d.\n", Delete(heap));
